eb1707f21dcdccaf775cfb378df61ce9732def9e
HuyTyskland <ndhuy@fossil.com>
2019-12-11 18:26:56 -0800

    [intern] changes

--- ports/stm32/mpconfigport.h
+++ ports/stm32/mpconfigport.h
@@ -212,8 +212,16 @@ extern const struct _mp_obj_module_t mp_module_network;
 extern const struct _mp_obj_module_t mp_module_onewire;
 extern const struct _mp_obj_module_t mp_module_lvgl;
 extern const struct _mp_obj_module_t mp_module_lvesp32;
-extern const struct _mp_obj_module_t mp_module_ILI9341;
+//extern const struct _mp_obj_module_t mp_module_ILI9341;
 extern const struct _mp_obj_module_t mp_module_rtch;
+
+#if MICROPY_PY_LVGL
+#define MICROPY_PORT_LVGL_DEF \
+   { MP_OBJ_NEW_QSTR(MP_QSTR_lvgl), (mp_obj_t)&mp_module_lvgl},  \
+   { MP_OBJ_NEW_QSTR(MP_QSTR_lvesp32), (mp_obj_t)&mp_module_lvesp32},
+#else
+#define MICROPY_PORT_LVGL_DEF
+#endif
 
 #if MICROPY_PY_STM
 #define STM_BUILTIN_MODULE               { MP_ROM_QSTR(MP_QSTR_stm), MP_ROM_PTR(&stm_module) },
@@ -247,13 +255,7 @@ extern const struct _mp_obj_module_t mp_module_rtch;
     SOCKET_BUILTIN_MODULE \
     NETWORK_BUILTIN_MODULE \
     { MP_ROM_QSTR(MP_QSTR__onewire), MP_ROM_PTR(&mp_module_onewire) }, \
-
-#define MICROPY_PORT_BUILTIN_MODULE_WEAK_LINKS \
-    { MP_OBJ_NEW_QSTR(MP_QSTR_lvgl), (mp_obj_t)&mp_module_lvgl},  \
-    { MP_OBJ_NEW_QSTR(MP_QSTR_lvesp32), (mp_obj_t)&mp_module_lvesp32},  \
-    { MP_OBJ_NEW_QSTR(MP_QSTR_ILI9341), (mp_obj_t)&mp_module_ILI9341},  \
-    { MP_OBJ_NEW_QSTR(MP_QSTR_rtch), (mp_obj_t)&mp_module_rtch},  \
-
+    MICROPY_PORT_LVGL_DEF
 
 #if MICROPY_PY_LVGL
 #include "lib/lv_bindings/lvgl/src/lv_misc/lv_gc.h"
@@ -286,9 +288,11 @@ struct _mp_bluetooth_nimble_root_pointers_t;
 #define MICROPY_PORT_ROOT_POINTERS \
     LV_ROOTS \
     \
-    mp_obj_t machine_pin_irq_handler[40]; \
+    void *mp_lv_user_data; \
     \
     const char *readline_hist[8]; \
+    \
+    mp_obj_t machine_pin_irq_handler[40]; \
     \
     mp_obj_t pyb_hid_report_desc; \
     \
